d6b0d422d3f1a286b874ad3f6463319a561a32e1
Vitor Costa <falkinerfx@gmail.com>
2024-02-06 17:11:56 -0300

    Cover animation fps dropping fixed

--- titleAnimation.js
+++ titleAnimation.js
@@ -1,6 +1,6 @@
 var titleAnimation = bodymovin.loadAnimation({
         container: document.getElementById("animation_container"),
-        path: 'https://lottie.host/a778160e-9da4-40b0-82ea-40dd1f4a0226/qDe8BYONMD.json',
+        path: 'https://lottie.host/2e7820d8-166d-4c69-90a5-5fa7b17fd1b1/izSZIp9nqO.json',
         rendererSettings: {
         viewBoxSize: false,
         preserveAspectRatio: 'xMidYMid slice'
